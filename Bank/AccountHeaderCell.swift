678a54f7d40db6d12caf3e4de81098abd513d6d3
Paul Herz <phrz@me.com>
2016-03-28 15:23:58 -0500

    WIP Account detail screen

---
+++ Bank/AccountHeaderCell.swift
@@ -0,0 +1,13 @@
+//
+//  AccountHeaderCell.swift
+//  Bank
+//
+//  Created by Paul Herz on 3/28/16.
+//  Copyright © 2016 Paul Herz. All rights reserved.
+//
+
+import UIKit
+
+class AccountHeaderCell: UITableViewCell {
+	
+}
